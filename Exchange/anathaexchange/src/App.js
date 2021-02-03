f690bb6c6f58296d530b4053cb5715b170e559ee
jayper <jay.sanchez@intelligentimagingsystems.com>
2021-02-02 17:19:40 -0700

    saving

--- Exchange/anathaexchange/src/App.js
+++ Exchange/anathaexchange/src/App.js
@@ -8,6 +8,8 @@ import Web3 from 'web3';
 let web3 = new Web3(window.ethereum);
 require('dotenv').config();
     
+
+
 function addToPool() {
   //alert(`addToPool`)
   const web3 = new Web3(Web3.currentProvider || "https://ropsten.infura.io/v3/6fd2fd8e1b334661b0c38556bd48b257")
@@ -30,51 +32,6 @@ function transferToken() {
   })
 }
 
-function parabellumAdd() {
-  const parabellumAddress = process.env.ZAPIN_MAINNET;
-  const parabellumContract = new web3.eth.Contract(parabellumcontractJSON, '0x4365E89B60B08595c49b0F94106C9c773750Da37');
-  window.web3 = new Web3(Web3.givenProvider || "https://ropsten.infura.io/v3/6fd2fd8e1b334661b0c38556bd48b257");
-  //https://ropsten.api.0x.org/swap/v1/quote?sellToken=0xc778417e063141139fce010982780140aa0cd5ab&buyToken=0x4A6a2F8c7b5F3e756868bc9AA24693aDb17f710f&buyAmount=1000000000000000
-  const URL = `https://api.0x.org/swap/v1/quote?sellToken=ETH&buyToken=USDC&buyAmount=1000000000000000`;
-
-  fetch(URL, { method:"GET",headers:{Accept:'application/json','Content-Type':'applicaiton/json'} })
-  .then((response) => 
-      //console.log(response.json())
-      //parse out data for swapData
-      response.json()
-  )
-  .then((response) => {
-    //console.log(response.data)
-    return response.data;
-  })
-  .then((swapData) => {
-    console.log(swapData);
-    window.ethereum.enable();
-    window.web3.eth.sendTransaction({
-      from: '0x923359F72080CFE2647711eD7BCb8fC25E938B7e',
-      to: '0x4A6a2F8c7b5F3e756868bc9AA24693aDb17f710f',
-      value:1000000000000000,
-      gasPrice:68000000,
-      gasLimit:1000000,
-      data: parabellumContract.methods.ZapIn(
-        '0xc778417e063141139fce010982780140aa0cd5ab',
-        '0x4A6a2F8c7b5F3e756868bc9AA24693aDb17f710f',
-        1000000000000000,
-        1262872576,
-        '0xfb2dd2a1366de37f7241c83d47da58fd503e2c64',
-        '0xfb2dd2a1366de37f7241c83d47da58fd503e2c64',
-        swapData
-      ).encodeABI()
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }).catch((error) => {
-    console.log(error);
-  })
-  
-  
-}
 
 
 function App() {
@@ -88,6 +45,8 @@ function App() {
   const [swapTarget, setswapTarget] = useState('0xFb2DD2A1366dE37f7241C83d47DA58fd503E2C64');
   const [swapData, setswapData] = useState();
   const [httpprovider, sethttpprovider] = useState("https://ropsten.infura.io/v3/6fd2fd8e1b334661b0c38556bd48b257")
+  const [txreceipt, settxreceipt] = useState();
+  
  
   if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
@@ -96,6 +55,57 @@ function App() {
     web3 = new Web3(new Web3.providers.HttpProvider(httpprovider));
   }
 
+  function parabellumAdd() {
+    const parabellumAddress = process.env.ZAPIN_MAINNET;
+    const parabellumContract = new web3.eth.Contract(parabellumcontractJSON, '0x4365E89B60B08595c49b0F94106C9c773750Da37');
+    window.web3 = new Web3(Web3.givenProvider || "https://ropsten.infura.io/v3/6fd2fd8e1b334661b0c38556bd48b257");
+    //https://ropsten.api.0x.org/swap/v1/quote?sellToken=0xc778417e063141139fce010982780140aa0cd5ab&buyToken=0x4A6a2F8c7b5F3e756868bc9AA24693aDb17f710f&buyAmount=1000000000000000
+    const URL = `https://ropsten.api.0x.org/swap/v1/quote?sellToken=ETH&buyToken=USDC&buyAmount=1000000000000000`;
+  
+    fetch(URL, { method:"GET",headers:{Accept:'application/json','Content-Type':'applicaiton/json'} })
+    .then((response) => 
+        //console.log(response.json())
+        //parse out data for swapData
+        response.json()
+    )
+    .then((response) => {
+      //console.log(response.data)
+      return response.data;
+    })
+    .then((swapData) => {
+      console.log(swapData)
+      window.ethereum.enable();
+      window.web3.eth.sendTransaction({
+        from: {FromTokenContractAddress},
+        to: {pairAddress},
+        value:{amount},
+        gasPrice:68000000,
+        gasLimit:1000000,
+        data: parabellumContract.methods.ZapIn(
+          {FromTokenContractAddress},
+          {pairAddress},
+          {amount},
+          {minPoolTokens},
+          {allowanceTarget},
+          {swapTarget},
+          swapData
+        ).encodeABI()
+      })
+      .then((tx) => {
+        //settxreceipt(tx);
+        console.log(tx)
+      })
+      .catch((error) => {
+        //settxreceipt(error)
+        console.log(error)
+      })
+    }).catch((error) => {
+      //settxreceipt(error);
+      console.log(error);
+    })
+    
+    
+  }
   
  
   return (
@@ -125,6 +135,10 @@ function App() {
                 </div>
 
                   <button id="addliquidity" onClick={parabellumAdd}>Add To Pool</button>
+
+                <div>
+                  {txreceipt}
+                </div>
     </div>
   );
 }
