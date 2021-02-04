92c34e1a291bdd38ddf16b16afc2bb4e4b7c4caa
jayper <jay.sanchez@intelligentimagingsystems.com>
2021-02-03 18:52:36 -0700

    saving changes

--- Exchange/anathaexchange/src/App.js
+++ Exchange/anathaexchange/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+
 import erc20contractJSON from './data/ERC20.json';
 import parabellumcontractJSON from './data/Parabellum_In_General_V3_0_1.json';
 import React, { useState } from 'react'
@@ -7,6 +8,7 @@ import { InjectedConnector } from '@web3-react/injected-connector'
 import Web3 from 'web3';
 let web3 = new Web3(window.ethereum);
 require('dotenv').config();
+const BigNumber = require('bignumber.js');
     
 
 
@@ -26,16 +28,17 @@ function addToPool() {
 
 function App() {
 
+  let _amount = new BigNumber(1000000000000000)
+  let _slippage = new BigNumber(16102705757875959344)
   /* set all data in these state variables */
-  const [FromTokenContractAddress, setFromTokenContractAddress] = useState('0xc778417e063141139fce010982780140aa0cd5ab');
-  const [pairAddress, setpairAddress] = useState('0x4A6a2F8c7b5F3e756868bc9AA24693aDb17f710f');
-  const [amount, setamount] = useState(1000000000000000); //0.001
-  const [minPoolTokens, setminPoolTokens] = useState(1262872576);
-  const [allowanceTarget, setallowanceTarget] = useState('0xFb2DD2A1366dE37f7241C83d47DA58fd503E2C64');
-  const [swapTarget, setswapTarget] = useState('0xFb2DD2A1366dE37f7241C83d47DA58fd503E2C64');
+  const [FromTokenContractAddress, setFromTokenContractAddress] = useState('0x0000000000000000000000000000000000000000');
+  const [pairAddress, setpairAddress] = useState('0x648450d9c30b73e2229303026107a1f7eb639f6c');
+  const [amount, setamount] = useState(_amount); 
+  const [minPoolTokens, setminPoolTokens] = useState(_slippage);
+  const [allowanceTarget, setallowanceTarget] = useState('0xdef1c0ded9bec7f1a1670819833240f027b25eff');
+  const [swapTarget, setswapTarget] = useState('0xdef1c0ded9bec7f1a1670819833240f027b25eff');
   const [swapData, setswapData] = useState();
   const [httpprovider, sethttpprovider] = useState("https://ropsten.infura.io/v3/6fd2fd8e1b334661b0c38556bd48b257")
-  const [txreceipt, settxreceipt] = useState();
   
   
  
@@ -62,10 +65,8 @@ function App() {
     window.ethereum.enable()
     const parabellumAddress = '0x4365E89B60B08595c49b0F94106C9c773750Da37';
     const parabellumContract = new web3.eth.Contract(parabellumcontractJSON, '0x4365E89B60B08595c49b0F94106C9c773750Da37');
-    window.web3 = new Web3(Web3.givenProvider || "https://ropsten.infura.io/v3/6fd2fd8e1b334661b0c38556bd48b257");
-    //https://ropsten.api.0x.org/swap/v1/quote?sellToken=0xc778417e063141139fce010982780140aa0cd5ab&buyToken=0x4A6a2F8c7b5F3e756868bc9AA24693aDb17f710f&buyAmount=1000000000000000
-    //const URL = `https://ropsten.api.0x.org/swap/v1/quote?sellToken=ETH&buyToken=USDC&buyAmount=1000000000000000`;
-    const URL = `https://api.0x.org/swap/v1/quote?sellToken=0x0000000000000000000000000000000000000000&buyToken=0xdac17f958d2ee523a2206206994597c13d831ec7&buyAmount=1000000000000000`;
+    window.web3 = new Web3(Web3.givenProvider || "https://mainnet.infura.io/v3/6fd2fd8e1b334661b0c38556bd48b257");
+    const URL = `https://api.0x.org/swap/v1/quote?sellToken=ETH&buyToken=WETH&buyAmount=100000000000000`;
     let coinbase;
 
     /* DEBUG */
@@ -73,9 +74,7 @@ function App() {
       coinbase = accounts[0]
       console.log(coinbase)
     })
-    //console.log(FromTokenContractAddress)
-    /********/
-  
+    
     fetch(URL, { method:"GET",headers:{Accept:'application/json','Content-Type':'applicaiton/json'} })
     .then((response) => 
         //console.log(response.json())
@@ -83,18 +82,18 @@ function App() {
         response.json()
     )
     .then((response) => {
-      //console.log(response.data)
+      console.log(response)
       return response.data;
     })
     .then((swapData) => {
-      console.log(swapData)
+      //console.log(swapData)
       window.ethereum.enable();
       window.web3.eth.sendTransaction({
         from: coinbase,
         to: pairAddress,
         value:amount,
-        gasPrice:68000000,
-        gasLimit:1000000,
+        gasPrice:160000000000,
+        gasLimit:200000000000,
         data: parabellumContract.methods.ZapIn(
           FromTokenContractAddress,
           pairAddress,
@@ -106,16 +105,19 @@ function App() {
         ).encodeABI()
       })
       .then((tx) => {
-        //settxreceipt(tx);
+        
         console.log(tx)
+        //settxreceipt(tx);
       })
       .catch((error) => {
-        //settxreceipt(error)
+        
         console.log(error)
+        //settxreceipt(error)
       })
     }).catch((error) => {
-      //settxreceipt(error);
+      
       console.log(error);
+      //settxreceipt(error);
     })
     
     
@@ -148,11 +150,7 @@ function App() {
                     </div>
                 </div>
 
-                  <button id="addliquidity" onClick={parabellumAdd}>Add To Pool</button>
-
-                <div>
-                  {txreceipt}
-                </div>
+                <button id="addliquidity" onClick={parabellumAdd}>Add To Pool</button>  
     </div>
   );
 }
